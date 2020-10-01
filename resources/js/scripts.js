cc059f85e25848f033be246cc1f17d18d414025b
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-09-30 21:09:27 -0400

    assignment completed

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -10,12 +10,12 @@ peopleInput.addEventListener('input', handleBill)
 
 function handleBill() {
     const bill = parseFloat(billInput.value)
-    const percent = parseInt(tipInput.value)
+    const percent = parseFloat(tipInput.value) // Change it to parseFloat because is more realistic.
     const persons = parseInt(peopleInput.value)
 
     const tip = bill* percent / 100
-    const total = bill + tip
+    const total = (bill + tip) / persons
 
-    displayTip.innerHTML = tip
-    displayTotal.innerHTML = total
+    displayTip.innerHTML = tip.toFixed(2)
+    displayTotal.innerHTML = total.toFixed(2)
 }
